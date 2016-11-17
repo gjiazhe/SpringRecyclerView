a8e30819e5a396c040df5934ffb81a73ae49b9dc
郭佳哲 <346672577@qq.com>
2016-11-17 23:14:54 +0800

    add sample

--- app/src/main/java/com/gjiazhe/springrecyclerview/sample/MainActivity.java
+++ app/src/main/java/com/gjiazhe/springrecyclerview/sample/MainActivity.java
@@ -2,12 +2,55 @@ package com.gjiazhe.springrecyclerview.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.gjiazhe.springrecyclerview.SpringRecyclerView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private SpringRecyclerView springRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        springRecyclerView = (SpringRecyclerView) findViewById(R.id.spring_recycler_view);
+        springRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        springRecyclerView.setAdapter(new MyAdapter());
+    }
+
+    class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+            View view = inflater.inflate(android.R.layout.simple_list_item_1, null);
+            return new MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.textView.setText("Item " + (position + 1));
+        }
+
+        @Override
+        public int getItemCount() {
+            return 20;
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            TextView textView;
+            MyViewHolder(View itemView) {
+                super(itemView);
+                textView = (TextView) itemView.findViewById(android.R.id.text1);
+            }
+        }
     }
 }
